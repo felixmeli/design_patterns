0b90dfdaca1de24ae0bb1ef166e7a5cadea3b649
Felix <ftoustau@gmail.com>
2022-07-05 14:18:17 -0300

    layout

--- app/src/main/java/com/mercadolibre/android/nfc/designpatterns/MainActivity.kt
+++ app/src/main/java/com/mercadolibre/android/nfc/designpatterns/MainActivity.kt
@@ -2,10 +2,24 @@ package com.mercadolibre.android.nfc.designpatterns
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var button: View
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button = findViewById(R.id.button)
+
+    }
+
+    companion object {
+        const val EVENT_DATA_INIT = "El usuario llego a la pantalla"
+        const val EVENT_DATA_CLICK = "El Usuario Hace click"
+        const val DEEPLINK = "meli://nfc/nfc_payments"
     }
 }
